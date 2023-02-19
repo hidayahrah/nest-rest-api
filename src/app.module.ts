ed08ddfe3d029e03be0509f10d2305786e547868
hidayahrah <hidayahrah97@gmail.com>
2023-02-19 15:53:57 +0800

    Configured dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import config from './config/keys';
+require('dotenv').config();
 
 @Module({
-  imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI)],
+  imports: [ItemsModule, MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
