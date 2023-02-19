1aa966b673a886e73fc2b649f8aa5dd08b4fa335
hidayahrah <hidayahrah97@gmail.com>
2023-02-19 14:57:51 +0800

    Added env

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
 export default {
     // mongoURI: 'mongodb://localhost:27017/nest-rest-api',
-    mongoURI: "mongodb+srv://hidayahrah:admin@hidayahrah.5thvne7.mongodb.net/?retryWrites=true&w=majority"
+    mongoURI: process.env.MONGO_URI,
 }
